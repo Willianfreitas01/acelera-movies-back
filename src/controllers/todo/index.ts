095238fcd779c8db2be20b9417677fd5368e3b6c
willian <willian.chris.89@gmail.com>
2022-08-04 12:00:50 -0300

    criação de delete e correção de outros endpoints

--- src/controllers/todo/index.ts
+++ src/controllers/todo/index.ts
@@ -2,6 +2,7 @@ import { response } from "express"
 import { request } from "http"
 import { getRepository } from "typeorm"
 import { Movie } from "@models/entity/ToDo"
+import { Login } from "@models/entity/login"
 
 export const itsWorks = (request, response) => {
   return response.json({ message: "It's Works!!" })
@@ -10,15 +11,9 @@ export const getTODO = (request, response) => {
   return response.json({ todos: [] })
 }
 export const postMovies = async (request, response) => {
-  try {
-    const movieRepository = getRepository(Movie)
-    const movieEntity = movieRepository.create(request.body)
-    const movieEntitySaved = await movieRepository.save(movieEntity)
-    console.log(movieEntitySaved)
-    return response.json(movieEntitySaved)
-  } catch (error) {
-    console.error("alguma coisa")
-  }
+  const movieRepository = getRepository(Movie)
+  const movieEntitySaved = await movieRepository.save(request.body)
+  return response.json(movieEntitySaved)
 }
 export const getMovies = async (request, response) => {
   const movieRepo = getRepository(Movie)
@@ -26,14 +21,20 @@ export const getMovies = async (request, response) => {
   return response.json(moviesFind)
 }
 
-export const getLogin = (request, response) => {
-  return response.json({ user: [] })
-}
-export const getMoviesId = async (request, response) => {
-  const movieRepo = getRepository(Movie)
-  const moviesFind = await movieRepo.find()
-  const { id } = request.params.id
-  return response.json(id, { movies: moviesFind })
+export const getLogin = async (request, response) => {
+  const loginRepository = getRepository(Login)
+  const login = await loginRepository.find()
+  return response.json(login)
 }
 
-export const deleteMovies = (request, response) => {}
+export const getMoviesId = async (request, response) => {
+  const movieRepo = getRepository(Movie)
+  const moviesFind = await movieRepo.findOne(request.params.id)
+  return response.json(moviesFind)
+}
+
+export const deleteMovies = async (request, response) => {
+  const pullMovies = getRepository(Movie)
+  const moviesDelete = await pullMovies.delete(request.body)
+  return response.json(moviesDelete)
+}
